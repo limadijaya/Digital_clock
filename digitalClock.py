7e4e29f85e481797b8d0a823392204bdc9664636
limadijaya <limasuhendra@gmail.com>
2022-04-21 23:43:45 +0700

    added digital clock to the screen

--- digitalClock.py
+++ digitalClock.py
@@ -1,11 +1,12 @@
 from tkinter import Tk, Label
+from datetime import datetime
 
-windows = TK()
+window = Tk()
 window.title("Digital Clock")
-window.geometry*"600x300")
+window.geometry("600x300")
 window.configure(bg="steelblue")
 
-label = Label(window, text="Welcome!",font=(Arial Black",78,"bold")
+label = Label(window, text="Welcome!",font=("Arial Black",78,"bold"))
 label.pack(pady=100)
 
 window.mainloop()

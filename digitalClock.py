58bea8e55d190c1b62134018005c094899ba08c1
limadijaya <limasuhendra@gmail.com>
2022-04-21 23:24:28 +0700

    added a welcome message

--- digitalClock.py
+++ digitalClock.py
@@ -1,7 +1,11 @@
 from tkinter import Tk, Label
 
 windows = TK()
-window.title("Digital Clock");
+window.title("Digital Clock")
 window.geometry*"600x300")
+window.configure(bg="steelblue")
+
+label = Label(window, text="Welcome!",font=(Arial Black",78,"bold")bg="steelblue")
+label.pack(pady=100)
 
 window.mainloop()

d20e16010d1e825f51f648c93cb579e3701eef61
limadijaya <limasuhendra@gmail.com>
2022-04-26 23:39:12 +0700

    added digital clock

--- digitalClock.py
+++ digitalClock.py
@@ -10,4 +10,10 @@ window.geometry("600x300")
 label = Label(window, text="Welcome!",font=("Arial Black",78,"bold"),bg="steelblue")
 label.pack(pady=100)
 
+def clock():
+    time = datetime.now().strftime("%H:%M:%S")
+    label.configure(text=time)
+    label.after(500,clock)
+
+clock()
 window.mainloop()

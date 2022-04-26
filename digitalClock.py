8748a6c71c3dbd2f30b4d988fb47f92825b53c91
limadijaya <limasuhendra@gmail.com>
2022-04-26 22:22:24 +0700

    Modification on April 26, 2022

--- digitalClock.py
+++ digitalClock.py
@@ -4,9 +4,10 @@ from datetime import datetime
 window = Tk()
 window.title("Digital Clock")
 window.geometry("600x300")
-window.configure(bg="steelblue")
+# window.configure(bg="steelblue")
+# window.configure(bg="blue")
 
-label = Label(window, text="Welcome!",font=("Arial Black",78,"bold"))
+label = Label(window, text="Welcome!",font=("Arial Black",78,"bold"),bg="steelblue")
 label.pack(pady=100)
 
 window.mainloop()
